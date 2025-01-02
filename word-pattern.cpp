911cbc7d86a05e651c530dc3e334bed78ddcab26
K-Preetham-Reddy <kallemp.reddy9@gmail.com>
2025-01-02 18:52:27 +0530

    Add word-pattern.cpp

---
+++ word-pattern.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+         String[] words= s.split(" ");
+         HashMap<Character,String> map=new HashMap<>();
+         if(pattern.length()!=words.length){
+            return false;
+         }
+         for(int i=0;i<pattern.length();i++){
+            if(map.containsKey(pattern.charAt(i))){
+                if(!map.get(pattern.charAt(i)).equals(words[i])){
+                    return false;
+                }
+            }else if(map.containsValue(words[i])){
+                return false;
+            }else{
+                map.put(pattern.charAt(i),words[i]);
+            }
+         }
+         return true;
+    }
+}
