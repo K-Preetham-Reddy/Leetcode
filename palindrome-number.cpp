fbbaa2a68d29f60e3e91cf76d27a0ed33c197a69
K-Preetham-Reddy <kallemp.reddy9@gmail.com>
2025-01-24 21:52:55 +0530

    Add palindrome-number.cpp

---
+++ palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int rev=0;
+        int y=x;
+        while(y>0){
+            rev=(rev*10)+(int)(y%10);
+            y/=10;
+        }
+        return(x==rev);
+        
+
+    }
+}
