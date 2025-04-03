3bd7f2f96cd0f2946ba711786e4dcadf8bdcaf42
K-Preetham-Reddy <kallemp.reddy9@gmail.com>
2025-04-03 21:25:07 +0530

    Add triangle.java

---
+++ triangle.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int len=triangle.size();
+        int[][] dp=new int[len+1][len+1];
+        for(int i=len-1;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                dp[i][j]=triangle.get(i).get(j)+Math.min(dp[i+1][j],dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
+    }
+}
